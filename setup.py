3b38553c36565da1fa62ee8df959015da90d0bab
Stamtiniakos <stam.tiniakos@gmail.com>
2020-11-17 21:18:51 +0000

    4th commit

--- setup.py
+++ setup.py
@@ -9,15 +9,18 @@ with open('requirements.txt') as f:
 setup(
     name="sortingAlgorithms", # Replace with your own username
     version="1.0.0",
+    #install_requires = ['requests']
+    install_requires=requires,
     author="Stamatis Tiniakos",
     author_email="stam.tiniakos@gmail.com",
     description="Python array sorting Algorithms",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/StamatisTiniakos/sortingAlgorithms/",
-    download_url = 'https://github.com/StamatisTiniakos/sortingAlgorithms/archive/master.zip'
+    download_url = 'https://github.com/StamatisTiniakos/sortingAlgorithms/archive/main.zip'
     #packages=['sortingAlgorithms'],
     packages = find_packages(exclude=['tests*']),
+    keywords = ['sorting','algorithm','python'],
     include_package_data=True,
     license = 'MIT License: http://opensource.org/licenses/MIT',
     classifiers=[
